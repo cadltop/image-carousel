dafcceb9e10f80a3042c113a04fa6a29617a93fa
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-29 22:51:57 -0500

    Select arrow buttons and send to carousel object

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import imageCarousel from "./imageCarousel";
 
 const images = document.querySelectorAll("div#slide > img");
+const arrowButtons = document.querySelectorAll('img[src$=".svg"]');
 
-const carCollection = imageCarousel(images);
+const carCollection = imageCarousel(images, arrowButtons[0], arrowButtons[1]);
 carCollection.showImage();

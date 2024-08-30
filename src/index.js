e0195e046daf2c86e6f55660701ebd9d25b4d1b0
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-29 22:18:55 -0500

    Create object to create a carousel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import 'normalize.css';
-import './index.css';
+import "normalize.css";
+import "./index.css";
+import imageCarousel from "./imageCarousel";
+
+const images = document.querySelectorAll("div#slide > img");
+
+const carCollection = imageCarousel(images);
+carCollection.showImage();

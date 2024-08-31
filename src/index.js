b53909fa8cc1cc0e079dc522b9f790b4e2f9b322
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-31 11:16:17 -0500

    call imageCarousel instead of declaring constant

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import imageCarousel from "./imageCarousel";
 const images = document.querySelectorAll("div#slide > img");
 const arrowButtons = document.querySelectorAll('img[src$=".svg"]');
 
-const carCollection = imageCarousel(images, arrowButtons[0], arrowButtons[1]);
+imageCarousel(images, arrowButtons[0], arrowButtons[1]);
